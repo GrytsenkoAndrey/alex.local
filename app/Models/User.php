d4895439a2e27f03a76604f803225917b33dbd14
Andrey Grytsenko <andreyapg@gmail.com>
2022-12-22 17:06:49 +0200

    Add User local scope 2022-12-22

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -43,4 +44,38 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeSearchFields(Builder $query, ?string $search = null): void
+    {
+        if (is_null($search)) {
+            $search = '';
+        }
+
+        $quantity = max(count(explode(' ', trim($search, ' '))), 1);
+
+        $first = $last = '';
+        if ($quantity === 2) {
+            list($first, $last) = explode(' ', $search);
+        }
+
+        $query
+            ->when(
+                $quantity === 2,
+                function ($query) use ($first, $last) {
+                    $query->where('name', 'LIKE', '%' . trim($first, ' ') . '%')
+                        ->where('lastName', 'LIKE', '%' . trim($last, ' ') . '%');
+                }
+            )
+            ->when(
+                $quantity <> 2,
+                function ($query) use ($search) {
+                    $query
+                        ->where('id', $search)
+                        ->orWhere('name', 'LIKE', '%' . trim($search, ' ') . '%')
+                        ->orWhere('lastName', 'LIKE', '%' . trim($search, ' ') . '%')
+                        ->orWhere('email', 'LIKE', '%' . trim($search, ' ') . '%')
+                        ->orWhere('phone', 'LIKE', '%' . trim($search, ' ') . '%');
+                }
+            );
+    }
 }
